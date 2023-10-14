26c918a95b2eeb7a7e7317224abc32fbf0bd4ee5
CosminGGeorgescu <cosmingg2013@gmail.com>
2023-10-14 22:57:39 +0300

    lab1/1

---
+++ lab1/1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+unordered_map<int, vector<int>> graph;
+// Repartizarea nodurilor
+vector<int> teams;
+
+bool dfs(int node, int team) {
+    teams[node] = team;
+    for (const int& neighbor : graph[node]) {
+        // Dam fail daca doua noduri vecine au aceeasi echipa 
+        // sau continuam colorarea si are un conflict at some point
+        if (teams[neighbor] == team || (teams[neighbor] == 0 && !dfs(neighbor, 3 - team))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n, m, a, b;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    teams.resize(n + 1, 0);
+
+    for (int i = 1; i <= n; ++i) {
+        // Incepem colorarea componentei conexe cu `1`
+        if (teams[i] == 0 && !dfs(i, 1)) {
+            cout << "IMPOSSIBLE" << endl;
+            return 0;
+        }
+    }
+   
+    for (int i = 1; i <= n; ++i) {
+        cout << teams[i] << " ";
+    }
+
+    return 0;
+}
