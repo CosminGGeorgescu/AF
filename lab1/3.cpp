4cf77dc21388cb3fea14f0878c7f28452628898f
CosminGGeorgescu <cosmingg2013@gmail.com>
2023-10-17 14:00:06 +0300

    lab1/3

---
+++ lab1/3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned short int n, m;
+char building[1000][1000];
+int rooms = 0;
+
+void fill(int i, int j) {
+    building[i][j] = '#';
+    if (i - 1 >= 0 && building[i - 1][j] == '.')
+        fill(i - 1, j);
+    if (j + 1 < m && building[i][j + 1] == '.')
+        fill(i, j + 1);
+    if (i + 1 < n && building[i + 1][j] == '.')
+        fill(i + 1, j);
+    if (j - 1 >= 0 && building[i][j - 1] == '.')
+        fill(i, j - 1);
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> building[i][j];
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            if (building[i][j] == '.'){
+                rooms++;
+                fill(i, j);
+            }
+
+    cout << rooms;
+
+    return 0;
+}
